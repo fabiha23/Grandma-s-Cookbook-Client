b88294c0cb4a45a072f834aed01d11bb128861f5
fabiha23 <fabiha.amatullah@gmail.com>
2025-05-21 14:19:15 +0600

    refactor: update navbar to show content conditionally for logged-in users

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import { IoMenu } from 'react-icons/io5';
 import { RxCross2 } from 'react-icons/rx';
 import { Link, NavLink } from 'react-router';
+import { AuthDataContext } from '../Contexts/AuthDataContext';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
+    const [openUser, setOpenUser] = useState(false)
+    const { user, loading } = use(AuthDataContext)
+
+    //     if (loading) {
+    //     return <div className='text-center py-5'>Loading...</div>; // or your spinner
+    // }
 
     const links = <>
         <li>
@@ -27,16 +34,20 @@ const Navbar = () => {
                 `mr-3 ${isActive ? 'text-primary' : 'text-accent hover:text-primary'}`
             } to='/my-recipe'>My Recipes</NavLink>
         </li>
-        <li>
-            <NavLink className={({ isActive }) =>
-                `mr-3 ${isActive ? 'text-primary' : 'text-accent hover:text-primary'}`
-            } to='/login'>Login</NavLink>
-        </li>
-        <li>
-            <NavLink className={({ isActive }) =>
-                `mr-3 ${isActive ? 'text-primary' : 'text-accent hover:text-primary'}`
-            } to='/register'>Register</NavLink>
-        </li>
+        {user ? (<></>) : (
+            <>
+                <li>
+                    <NavLink className={({ isActive }) =>
+                        `mr-3 ${isActive ? 'text-primary' : 'text-accent hover:text-primary'}`
+                    } to='/login'>Login</NavLink>
+                </li>
+                <li>
+                    <NavLink className={({ isActive }) =>
+                        `mr-3 ${isActive ? 'text-primary' : 'text-accent hover:text-primary'}`
+                    } to='/register'>Register</NavLink>
+                </li>
+            </>)
+        }
     </>
     return (
         <nav>
@@ -51,8 +62,21 @@ const Navbar = () => {
                         {links}
                     </ul>
                 </div>
-                <div className='flex'>
+                <div className='flex gap-2 items-center'>
                     <button>Toggle</button>
+                    {loading ? (
+                        <>Loading...</> // or a spinner component
+                    ) : user ? (
+                        <div className='relative'>
+                            <img onClick={() => setOpenUser(!openUser)} className='w-9 h-9 object-cover rounded-full cursor-pointer' src={user.photoURL} alt="User" />
+                            {openUser && (
+                                <div className="absolute right-0 mt-2 bg-base-100 p-3 shadow rounded z-50 text-accent text-lg border-2 border-[#D9CFC1] w-50">
+                                    <p>{user.displayName}</p>
+                                    <button>Logout</button>
+                                </div>
+                            )}
+                        </div>
+                    ) : null}
                     <span onClick={() => setOpen(!open)}>{
                         open ? <RxCross2 size={32} /> : <IoMenu size={32} className='xl:hidden' />
                     }
